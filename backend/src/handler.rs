e669be3e13a56ca16e0c76780c69aa830bd646d8
r4z4 <r4z4aa@gmail.com>
2023-09-28 21:17:14 -0500

    admin scope

--- backend/src/handler.rs
+++ backend/src/handler.rs
@@ -451,58 +451,6 @@ async fn upload(mut payload: Multipart, req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().into()
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ResponseUserList {
-    pub users: Vec<ResponseUser>,
-}
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ResponseUser {
-    pub user_id: i32,
-    pub username: String,
-    pub email: String,
-}
-
-#[get("/users")]
-pub async fn get_users_handler(
-    req: HttpRequest,
-    opts: web::Query<FilterOptions>,
-    data: web::Data<AppState>,
-    _: jwt_auth::JwtAuth,
-) -> impl Responder {
-    let ext = req.extensions();
-    let user_id = ext.get::<i32>().unwrap();
-    dbg!(user_id);
-    let limit = opts.limit.unwrap_or(10);
-    let offset = (opts.page.unwrap_or(1) - 1) * limit;
-
-    let query_result = sqlx::query_as!(
-        ResponseUser,
-        "SELECT user_id, username, email FROM users ORDER by user_id LIMIT $1 OFFSET $2",
-        limit as i32,
-        offset as i32
-    )
-    .fetch_all(&data.db)
-    .await;
-
-    if query_result.is_err() {
-        let message = "Error occurred while fetching all engagement items";
-        return HttpResponse::InternalServerError()
-            .json(json!({"status": "error","message": message}));
-    }
-
-    let users = query_result.unwrap();
-
-    let json_response = ResponseUserList { users: users };
-
-    // let json_response = serde_json::json!({
-    //     "status": "success",
-    //     "results": users.len(),
-    //     "engagements": users
-    // });
-    HttpResponse::Ok().json(json_response)
-    // HttpResponse::NoContent().finish()
-}
-
 #[get("/consults")]
 pub async fn get_consults_handler(
     opts: web::Query<FilterOptions>,
@@ -539,17 +487,6 @@ pub async fn get_consults_handler(
     // });
     HttpResponse::Ok().json(json_response)
     // HttpResponse::NoContent().finish()
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ResponseConsultantList {
-    pub consultants: Vec<ResponseConsultant>,
-}
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ResponseConsultant {
-    pub consultant_id: i32,
-    pub specialty_id: i32,
-    pub img_path: Option<String>,
 }
 
 #[get("/clients")]
@@ -602,44 +539,6 @@ pub struct ResponseClient {
     pub client_zip: String,
 }
 
-#[get("/consultants")]
-pub async fn get_consultants_handler(
-    opts: web::Query<FilterOptions>,
-    data: web::Data<AppState>,
-) -> impl Responder {
-    let limit = opts.limit.unwrap_or(10);
-    let offset = (opts.page.unwrap_or(1) - 1) * limit;
-
-    let query_result = sqlx::query_as!(
-        ResponseConsultant,
-        "SELECT consultant_id, specialty_id, img_path FROM consultants ORDER by consultant_id LIMIT $1 OFFSET $2",
-        limit as i32,
-        offset as i32
-    )
-    .fetch_all(&data.db)
-    .await;
-
-    if query_result.is_err() {
-        let message = "Error occurred while fetching all consultant records";
-        return HttpResponse::InternalServerError()
-            .json(json!({"status": "error","message": message}));
-    }
-
-    let consultants = query_result.unwrap();
-
-    let json_response = ResponseConsultantList {
-        consultants: consultants,
-    };
-
-    // let json_response = serde_json::json!({
-    //     "status": "success",
-    //     "results": users.len(),
-    //     "engagements": users
-    // });
-    HttpResponse::Ok().json(json_response)
-    // HttpResponse::NoContent().finish()
-}
-
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api")
         .service(health_checker_handler)
@@ -648,8 +547,6 @@ pub fn config(conf: &mut web::ServiceConfig) {
         .service(get_engagement_handler)
         .service(edit_engagement_handler)
         .service(delete_engagement_handler)
-        .service(get_users_handler)
-        .service(get_consultants_handler)
         .service(location_options_handler)
         .service(consultant_options_handler)
         .service(client_options_handler)
--- backend/src/scopes/admin.rs
+++ backend/src/scopes/admin.rs
@@ -1,19 +1,15 @@
-use actix_web::{get, web, HttpResponse, Responder, Scope};
+use actix_web::{get, web, HttpResponse, Responder, HttpRequest, HttpMessage, Scope};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
-use crate::{model::UserModel, schema::FilterOptions, AppState};
+use crate::{schema::FilterOptions, AppState, extractors::jwt_auth};
 
 pub fn admin_scope() -> Scope {
     web::scope("/admin")
         // .route("/users", web::get().to(get_users_handler))
-        .route("/users/{id}", web::get().to(get_user_handler))
+        .service(get_users_handler)
+        .service(get_consultants_handler)
 }
-
-// #[derive(Serialize, Deserialize)]
-// struct Response {
-//     message: String,
-// }
 
 #[derive(Serialize, Deserialize)]
 struct EncodeResponse {
@@ -26,6 +22,7 @@ pub struct Message {
     pub id: usize,
     pub content: String,
 }
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ResponseUserList {
     pub users: Vec<ResponseUser>,
@@ -35,6 +32,17 @@ pub struct ResponseUser {
     pub user_id: i32,
     pub username: String,
     pub email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ResponseConsultantList {
+    pub consultants: Vec<ResponseConsultant>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ResponseConsultant {
+    pub consultant_id: i32,
+    pub specialty_id: i32,
+    pub img_path: Option<String>,
 }
 
 // #[get("/users")]
@@ -75,20 +83,100 @@ pub struct ResponseUser {
 //     HttpResponse::NoContent().finish()
 // }
 
-async fn get_user_handler(path: web::Path<usize>) -> HttpResponse {
-    let id: usize = path.into_inner();
-    let content = String::from("Message Content");
-    let message: Message = Message { id, content };
-    HttpResponse::Ok().json(ResponseUser {
-        username: message.content,
-        user_id: 3,
-        email: "aa@aa.com".to_owned(),
-    })
+// async fn get_user_handler(path: web::Path<usize>) -> HttpResponse {
+//     let id: usize = path.into_inner();
+//     let content = String::from("Message Content");
+//     let message: Message = Message { id, content };
+//     HttpResponse::Ok().json(ResponseUser {
+//         username: message.content,
+//         user_id: 3,
+//         email: "aa@aa.com".to_owned(),
+//     })
+// }
+
+#[get("/users")]
+pub async fn get_users_handler(
+    req: HttpRequest,
+    opts: web::Query<FilterOptions>,
+    data: web::Data<AppState>,
+    _: jwt_auth::JwtAuth,
+) -> impl Responder {
+    let ext = req.extensions();
+    let user_id = ext.get::<i32>().unwrap();
+    dbg!(user_id);
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    let query_result = sqlx::query_as!(
+        ResponseUser,
+        "SELECT user_id, username, email FROM users ORDER by user_id LIMIT $1 OFFSET $2",
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await;
+
+    if query_result.is_err() {
+        let message = "Error occurred while fetching all engagement items";
+        return HttpResponse::InternalServerError()
+            .json(json!({"status": "error","message": message}));
+    }
+
+    let users = query_result.unwrap();
+
+    let json_response = ResponseUserList { users: users };
+
+    // let json_response = serde_json::json!({
+    //     "status": "success",
+    //     "results": users.len(),
+    //     "engagements": users
+    // });
+    HttpResponse::Ok().json(json_response)
+    // HttpResponse::NoContent().finish()
+}
+
+#[get("/consultants")]
+pub async fn get_consultants_handler(
+    opts: web::Query<FilterOptions>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    let query_result = sqlx::query_as!(
+        ResponseConsultant,
+        "SELECT consultant_id, specialty_id, img_path FROM consultants ORDER by consultant_id LIMIT $1 OFFSET $2",
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await;
+
+    if query_result.is_err() {
+        let message = "Error occurred while fetching all consultant records";
+        return HttpResponse::InternalServerError()
+            .json(json!({"status": "error","message": message}));
+    }
+
+    let consultants = query_result.unwrap();
+
+    let json_response = ResponseConsultantList {
+        consultants: consultants,
+    };
+
+    // let json_response = serde_json::json!({
+    //     "status": "success",
+    //     "results": users.len(),
+    //     "engagements": users
+    // });
+    HttpResponse::Ok().json(json_response)
+    // HttpResponse::NoContent().finish()
 }
 
 // pub fn config(conf: &mut web::ServiceConfig) {
 //     let scope = web::scope("/admin")
-//         .service(get_users_handler);
+//         .service(get_users_handler)
+//         .service(get_consultants_handler);
 
 //     conf.service(scope);
 // }
